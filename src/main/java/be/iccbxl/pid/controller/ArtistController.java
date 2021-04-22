0d9fea1a2a3563cc19ee84c886dadc3e342b3747
Victor Od <victor.od@icloud.com>
2021-04-22 21:55:18 +0200

    Résolution des erreurs de la classe ArtistContoller

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -1,14 +1,12 @@
-package be.iccbxl.pid.Controller;
+package be.iccbxl.pid.controller;
+
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import be.iccbxl.pid.model.Artist;
-import be.iccbxl.pid.model.ArtistService;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
@@ -19,14 +17,12 @@ public class ArtistController {
     ArtistService service;
 
     @GetMapping("/artists")
-    public String index() {
+    public String index(Model model) {
         List<Artist> artists = service.getAllArtists();
 
-        // erreur : model ??? a resoudre
-        /*
         model.addAttribute("artists", artists);
         model.addAttribute("title", "Liste des artistes");
-*/
+
         return "artist/index";
     }
 
