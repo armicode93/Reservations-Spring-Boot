cf03ff0452e2edce5dad201e6d2708208ecf999b
Victor Od <victor.od@icloud.com>
2021-04-22 17:05:30 +0200

    Creation de la classe ArtistController

---
+++ src/main/java/be/iccbxl/pid/Controller/ArtistController.java
@@ -0,0 +1,36 @@
+package be.iccbxl.pid.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.iccbxl.pid.model.Artist;
+import be.iccbxl.pid.model.ArtistService;
+
+@Controller
+public class ArtistController {
+    @Autowired
+    ArtistService service;
+
+    @GetMapping("/artists")
+    public String index() {
+        StringBuilder content = new StringBuilder("<ul>");
+
+        List<Artist> artists = service.getAllArtists();
+        artists.forEach(artist -> {
+            content.append("<li>"+artist+"</li>");
+        });
+        content.append("</ul>");
+
+        return content.toString();
+    }
+
+}
+
+
+
+
