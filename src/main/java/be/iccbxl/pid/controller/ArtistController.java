131839937bcee92265a4dedd81bc3a5a7c7070f9
naser <beab0907@student.iepscf-uccle.be>
2021-05-06 14:27:39 +0200

    Resolution probleme affichage show.html

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -25,6 +26,19 @@ public class ArtistController {
         model.addAttribute("title", "Liste des artistes");
 
         return "artist/index";
+    }
+
+    @GetMapping("/artists/{id}") //methode show pour recuperer un artiste avec son id e affiche dans show.html
+
+    public String show(Model model, @PathVariable("id") String id){ // path sert pour faire correspondre l'id de l'url avec celui de la methode show
+        Artist artist = service.getArtist(id);
+        //get artist est utilie pour recuperer l'artiste dont l-id correspond,
+
+        //nous l'ajoutons au modele avant de renvoyer le template show.html
+        model.addAttribute("artist",artist);
+        model.addAttribute("title", "Fiche d'un artiste");
+
+        return "artist/show";
     }
 
 }
