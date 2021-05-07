a904b388da908c3e1d1e4e69e93b8b1963c4c239
Victor Od <victor.od@icloud.com>
2021-05-08 00:15:57 +0200

    Creation des interfaces LocalityRepository, RoleRepository, TypeRepository dans model exo 18.1->chap2

---
+++ src/main/java/be/iccbxl/pid/model/TypeRepository.java
@@ -0,0 +1,7 @@
+package be.iccbxl.pid.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TypeRepository extends CrudRepository<Type, Long> {
+    Type findByType(String type);
+}
