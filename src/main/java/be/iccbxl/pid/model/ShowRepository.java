d98e4fc2a3bbb689f304eae00cae1efa02de494b
armicode93 <naserr93@hotmail.com>
2021-05-15 22:58:19 +0200

    Points 36 37, Creation templates du modele Location

--- src/main/java/be/iccbxl/pid/model/ShowRepository.java
+++ src/main/java/be/iccbxl/pid/model/ShowRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ShowRepository extends CrudRepository<Show, Long> {
     Show findBySlug(String slug);
-    Show findByTitre(String titre);
+    Show findByTitle(String titre);
     List<Show> findByLocation(Location location);
 }
 
